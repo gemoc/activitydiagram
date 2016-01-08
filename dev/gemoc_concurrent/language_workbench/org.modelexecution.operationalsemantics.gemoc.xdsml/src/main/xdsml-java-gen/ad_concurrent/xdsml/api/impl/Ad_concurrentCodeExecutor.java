54b46d15a994a59b519ae6caff418fe057c9363a
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-01-08 10:11:47 +0100

    adapting activitydiagram to refactored studio

--- dev/gemoc_concurrent/language_workbench/org.modelexecution.operationalsemantics.gemoc.xdsml/src/main/xdsml-java-gen/ad_concurrent/xdsml/api/impl/Ad_concurrentCodeExecutor.java
+++ dev/gemoc_concurrent/language_workbench/org.modelexecution.operationalsemantics.gemoc.xdsml/src/main/xdsml-java-gen/ad_concurrent/xdsml/api/impl/Ad_concurrentCodeExecutor.java
@@ -1,15 +1,15 @@
 /* GENERATED FILE, do not modify manually                                                    *
  * If you need to modify it, disable the generation in the BuildOptions of the project.xdsml */
 package ad_concurrent.xdsml.api.impl;
-import org.gemoc.executionengine.ccsljava.engine.dsa.executors.CodeExecutorDispatcher;
+import org.gemoc.execution.concurrent.ccsljavaengine.dsa.executors.CodeExecutorDispatcher;
 public class Ad_concurrentCodeExecutor extends CodeExecutorDispatcher 
-		implements org.gemoc.gemoc_language_workbench.extensions.k3.dsa.api.IK3DSAExecutorClassLoader  {
+		implements org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.api.IK3DSAExecutorClassLoader  {
 	public Ad_concurrentCodeExecutor(){
 	    // add K3 DSA specific executor
-		addExecutor(new org.gemoc.gemoc_language_workbench.extensions.k3.dsa.impl.Kermeta3AspectsCodeExecutor(this,
+		addExecutor(new org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.impl.Kermeta3AspectsCodeExecutor(this,
 			"org.modelexecution.operationalsemantics.k3.dynamic"));
 		// fall back executor : search classic java method
-		addExecutor(new org.gemoc.executionengine.ccsljava.engine.dsa.executors.JavaCodeExecutor());
+		addExecutor(new org.gemoc.execution.concurrent.ccsljavaengine.dsa.executors.JavaCodeExecutor());
 	}
 	public String getDSAProjectName(){
 		//TODO please implement
