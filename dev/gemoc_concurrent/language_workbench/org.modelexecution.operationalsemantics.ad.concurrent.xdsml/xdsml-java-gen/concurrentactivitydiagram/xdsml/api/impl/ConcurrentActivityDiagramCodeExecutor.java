5d8575c89d74fe509c3b695a082fece9a6ad61be
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-02-11 17:57:20 +0100

    adding feature and repository for deployeing ActivityDiagram Language deployer

--- dev/gemoc_concurrent/language_workbench/org.modelexecution.operationalsemantics.ad.concurrent.xdsml/xdsml-java-gen/concurrentactivitydiagram/xdsml/api/impl/ConcurrentActivityDiagramCodeExecutor.java
+++ dev/gemoc_concurrent/language_workbench/org.modelexecution.operationalsemantics.ad.concurrent.xdsml/xdsml-java-gen/concurrentactivitydiagram/xdsml/api/impl/ConcurrentActivityDiagramCodeExecutor.java
@@ -5,7 +5,7 @@ import org.gemoc.execution.concurrent.ccsljavaengine.dsa.executors.CodeExecutorD
 public class ConcurrentActivityDiagramCodeExecutor extends CodeExecutorDispatcher 
 		implements org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.api.IK3DSAExecutorClassLoader  {
 	public ConcurrentActivityDiagramCodeExecutor(){
-	    // add K3 DSA specific executor
+	    // add Melange or K3 DSA specific executors
 		addExecutor(new org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.impl.MelangeCodeExecutor(this,
 			"org.modelexecution.operationalsemantics.ad.concurrent.xdsml.ConcurrentActivityDiagram"));
 		// fall back executor : search classic java method
